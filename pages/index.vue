dbec4cc28390bb67852bf636078d5bd557cfc458
石亀広大 <kira5052@gmail.com>
2018-05-19 23:45:56 +0900

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,28 @@
 <template>
-  <section class="container">
+  <section class="">
     <div>
-      <app-logo/>
-      <h1 class="title">
+      <h1 class="">
         nuxt-pwa-template
       </h1>
-      <h2 class="subtitle">
+      <h2 class="">
         Nuxt.js project
       </h2>
-      <div class="links">
+      <div class="">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green">Documentation</a>
+          class="">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
-export default {
-  components: {
-    AppLogo
-  }
-}
 </script>
 
 <style>
