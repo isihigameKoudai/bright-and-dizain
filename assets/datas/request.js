3f5d5b717c38340889937b250566f78845232639
石亀広大 <kira5052@gmail.com>
2018-11-17 23:24:13 +0900

    git周りの整備

---
+++ assets/datas/request.js
@@ -0,0 +1,6 @@
+const slack = {
+  url: "https://hooks.slack.com/services/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",
+  header: { 'Content-type': 'application/json' }
+};
+
+export default { slack };
