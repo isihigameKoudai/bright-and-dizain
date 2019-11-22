8e7bd51c3b86baedfa335f16a57ac4376ae2ffd7
koudaiishigame <kira5052@gmail.com>
2019-11-23 02:10:43 +0900

    service層のts化

--- src/service/Contact.ts
+++ src/service/Contact.ts
@@ -3,6 +3,8 @@ import axios from 'axios'
 const SLACK_API_CODE = process.env.SLACK_API_CODE
 const API_URL = 'https://hooks.slack.com/services/'
 
+// type SubmitType = (company: String, name: String, email: String, tel: String, message: String) => Promise<T>;
+
 export default function submitContact({
   company = '',
   name = '',
@@ -11,10 +13,10 @@ export default function submitContact({
   message = '',
 }) {
 
-  const url = API_URL + SLACK_API_CODE;
-  const header = { 'Content-type': 'application/json' }
-  const contacts = `会社・組織：${company}\n名前：${name}\nEメール：${email}\n電話番号：${tel}\nメッセージ：${message}\n`
-  const options = {
+  const url: string = API_URL + SLACK_API_CODE;
+  const header: object = { 'Content-type': 'application/json' }
+  const contacts: string = `会社・組織：${company}\n名前：${name}\nEメール：${email}\n電話番号：${tel}\nメッセージ：${message}\n`
+  const options: object = {
     method: 'post',
     baseURL: url,
     header,
--- src/service/validation.ts
+++ src/service/validation.ts
@@ -1,14 +1,16 @@
-export const validCompany = value => {
+type ValidType = (value: string) => boolean
+
+export const validCompany: ValidType = value => {
+  const isErr: boolean = value.length < 4
+  return isErr;
+}
+
+export const validName: ValidType = value => {
   const isErr = value.length < 4
   return isErr;
 }
 
-export const validName = value => {
-  const isErr = value.length < 4
-  return isErr;
-}
-
-export const validEmail = value => {
+export const validEmail: ValidType = value => {
   const regex = new RegExp(
     /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
   )
@@ -16,12 +18,12 @@ export const validEmail = value => {
   return isErr;
 }
 
-export const validTel = value => {
+export const validTel: ValidType = value => {
   const isErr = value.length < 8 || isNaN(Number(value))
   return isErr;
 }
 
-export const validMessage = value => {
+export const validMessage: ValidType = value => {
   const isErr = value.length < 10;
   return isErr;
 }
