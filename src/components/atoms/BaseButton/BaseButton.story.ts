939c227f1e3c7d5383a2567e2c7ed303b2b17eb9
koudaiishigame <kira5052@gmail.com>
2019-11-30 11:46:15 +0900

    storybookの導入

---
+++ src/components/atoms/BaseButton/BaseButton.story.ts
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/vue'
+
+import BaseButton from './BaseButton.vue'
+
+storiesOf('BaseButton', module).add('simple', () => ({
+  components: { BaseButton },
+  template: `<BaseButton>Button</BaseButton>`
+}))
