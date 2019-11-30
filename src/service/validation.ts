0ad4259327ab885ec228f212fc86c395a57224a1
koudaiishigame <kira5052@gmail.com>
2019-11-30 16:27:57 +0900

    animation.jsにboolean付与

--- src/service/validation.ts
+++ src/service/validation.ts
@@ -6,7 +6,7 @@ export const validCompany: ValidType = value => {
 }
 
 export const validName: ValidType = value => {
-  const isErr = value.length < 4
+  const isErr: boolean = value.length < 4
   return isErr
 }
 
@@ -14,16 +14,16 @@ export const validEmail: ValidType = value => {
   const regex = new RegExp(
     /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
   )
-  const isErr = !regex.test(value)
+  const isErr: boolean = !regex.test(value)
   return isErr
 }
 
 export const validTel: ValidType = value => {
-  const isErr = value.length < 8 || isNaN(Number(value))
+  const isErr: boolean = value.length < 8 || isNaN(Number(value))
   return isErr
 }
 
 export const validMessage: ValidType = value => {
-  const isErr = value.length < 10
+  const isErr: boolean = value.length < 10
   return isErr
 }
