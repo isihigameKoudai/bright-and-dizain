c036ac1d0720845a62d2eddeb3acf2488c35639d
koudaiishigame <kira5052@gmail.com>
2020-03-28 01:51:50 +0900

    GAの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 
 const srcDir: string = 'src/'
 const SLACK_API_CODE: string = process.env.SLACK_API_CODE || ''
+const GA: string = process.env.GA || ''
 require('dotenv').config()
 
 const nuxtConfig: NuxtConfiguration = {
@@ -118,7 +119,13 @@ const nuxtConfig: NuxtConfiguration = {
   modules: [
     ['@nuxtjs/pwa', { icon: false }],
     '@nuxtjs/axios',
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: GA
+      }
+    ]
   ],
   buildModules: [
     [
@@ -130,7 +137,8 @@ const nuxtConfig: NuxtConfiguration = {
     ]
   ],
   env: {
-    SLACK_API_CODE: SLACK_API_CODE
+    SLACK_API_CODE,
+    GA
   },
   plugins: [{ src: '~/plugins/vue-awesome-swiper', ssr: false }],
   manifest: {
