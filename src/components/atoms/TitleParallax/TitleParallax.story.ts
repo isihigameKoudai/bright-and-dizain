02d1730e7e626a33efa444070bef40690ceb43e5
koudaiishigame <kira5052@gmail.com>
2020-07-09 01:08:35 +0900

    TitleParallaxのstory追加

---
+++ src/components/atoms/TitleParallax/TitleParallax.story.ts
@@ -0,0 +1,29 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs, text } from '@storybook/addon-knobs/vue';
+
+import TitleParallax from './TitleParallax.vue'
+
+// import { setParallax } from '../../../utils/parallax.ts'
+
+const decorator = () => ({
+  template: `<div style="padding-top: 300px; padding-bottom: 300px;">
+    <h1 style="text-align: center;">scroll</h1>
+    <story>
+  </div>`
+})
+
+storiesOf('atoms', module).addDecorator(decorator).addDecorator(withKnobs).add('TitleParallax', () => ({
+  components: { TitleParallax },
+  props: {
+    title: {
+      default: text('text', 'Title')
+    },
+    path: {
+      default: text('path', '/img/TitlePhotograph.jpg')
+    }
+  },
+  // mounted() {
+  //   setParallax();
+  // },
+  template: `<TitleParallax :path="path" :title="title" />`
+}))
