3f8b4907adce37654d8489290034757b819b67f4
koudaiishigame <kira5052@gmail.com>
2020-07-09 00:04:24 +0900

    LabelPageTopのstory追加

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure } from '@storybook/vue';
 
+import '../src/assets/css/reset.scss'
+
 const req = require.context("../src/components/", true, /\.story\.(tsx|ts)$/);
 
 function loadStories() {
