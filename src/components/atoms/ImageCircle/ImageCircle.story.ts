34a61be64e4c1b1c3cce8220673430ed1ade4f8d
koudaiishigame <kira5052@gmail.com>
2020-07-08 23:32:51 +0900

    ImageCircleのstorybook

---
+++ src/components/atoms/ImageCircle/ImageCircle.story.ts
@@ -0,0 +1,15 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs } from '@storybook/addon-knobs/vue';
+
+import ImageCircle from './ImageCircle.vue'
+import image from '../../../assets/img/portfolio/creative/c2.jpg';
+
+storiesOf('atoms', module).addDecorator(withKnobs).add('ImageCircle', () => ({
+  components: { ImageCircle },
+  props: {
+    path: {
+      default: image
+    }
+  },
+  template: `<ImageCircle :path="path" @click="onClick" />`
+}))
