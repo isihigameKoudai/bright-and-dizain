3be69e341402d5238d52a807f20da0ee113bad63
koudaiishigame <kira5052@gmail.com>
2020-07-09 00:13:11 +0900

    LogoMediaのstory追加

---
+++ src/components/atoms/LogoMedia/LogoMedia.story.ts
@@ -0,0 +1,15 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs, text } from '@storybook/addon-knobs/vue';
+
+import LogoMedia from './LogoMedia.vue'
+import logo from '../../../static/svg/LogoFacebook.svg'
+
+storiesOf('atoms', module).addDecorator(withKnobs).add('LogoMedia', () => ({
+  components: { LogoMedia },
+  props: {
+    path: {
+      default: logo
+    }
+  },
+  template: `<LogoMedia :path="path">{{ text }}</LogoMedia>`
+}))
