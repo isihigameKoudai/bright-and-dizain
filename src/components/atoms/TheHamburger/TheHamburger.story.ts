cbd650635d8ce5d831eeb4149299557180f2d847
koudaiishigame <kira5052@gmail.com>
2021-02-20 19:56:42 +0900

    humbueger fix

--- src/components/atoms/TheHamburger/TheHamburger.story.ts
+++ src/components/atoms/TheHamburger/TheHamburger.story.ts
@@ -1,22 +1,6 @@
-import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
-import { withKnobs, boolean } from '@storybook/addon-knobs/vue';
 
 import TheHamburger from './TheHamburger.vue'
-
-storiesOf('atoms', module).addDecorator(withKnobs).add('TheHamburger', () => ({
-  components: { TheHamburger },
-  props: {
-    isOpen: {
-      default: boolean('isOpen', false)
-    }
-  },
-  methods: {
-    onToggle: action('click')
-  },
-  template: `<TheHamburger :isActive="isOpen" @click="onToggle" />`
-}))
-
 
 export default {
   components: { TheHamburger },
