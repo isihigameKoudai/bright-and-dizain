f2ea2273dc482d0d3cce38cd170f8db380dca9c4
koudaiishigame <kira5052@gmail.com>
2021-02-20 17:24:39 +0900

    BaseTextAreaのCSF化

--- src/components/atoms/BaseTextArea/BaseTextArea.story.ts
+++ src/components/atoms/BaseTextArea/BaseTextArea.story.ts
@@ -1,18 +1,22 @@
 import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/vue'
-import { withKnobs, text } from '@storybook/addon-knobs/vue';
 
 import BaseTextArea from './BaseTextArea.vue'
 
-storiesOf('atoms', module).addDecorator(withKnobs).add('BaseTextArea', () => ({
+export default {
   components: { BaseTextArea },
-  props: {
-    placeHolder: {
-      default: text('placeHolder', 'placeholder')
-    }
-  },
+  title: 'atoms/BaseTextArea',
+  template: '<BaseTextArea />'
+}
+
+export const withPlaceHolder = (args, { argTypes }) => ({
+  components: { BaseTextArea },
+  template: '<BaseTextArea :place-holder="placeHolder" @text="onClick" />',
+  props: Object.keys(argTypes),
   methods: {
-    onClick: action('text', text)
-  },
-  template: `<BaseTextArea :place-holder="placeHolder" @text="onClick" />`
-}))
+    onClick: action('text')
+  }
+});
+
+withPlaceHolder.args = {
+  placeHolder: '入力してください',
+}
