6cbd158ee35e409eceb26f7a09dd0334fd4a7682
koudaiishigame <kira5052@gmail.com>
2021-02-20 18:15:45 +0900

    textBox CSF

--- src/components/atoms/BaseTextBox/BaseTextBox.story.ts
+++ src/components/atoms/BaseTextBox/BaseTextBox.story.ts
@@ -1,24 +1,33 @@
 import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/vue'
-import { withKnobs, text, boolean } from '@storybook/addon-knobs/vue';
 
 import BaseTextBox from './BaseTextBox.vue'
 
-storiesOf('atoms', module).addDecorator(withKnobs).add('BaseTextBox', () => ({
+export default {
   components: { BaseTextBox },
-  props: {
-    placeHolder: {
-      default: text('placeHolder', 'placeholder')
-    },
-    isInvalid: {
-      default: boolean('isInvalid', false)
-    },
-    value: {
-      default: text('value', '')
-    }
-  },
+  title: 'atoms/BaseTextBox',
+  template: '<BaseTextBox />'
+}
+
+export const withPlaceHolder = (args, { argTypes }) => ({
+  components: { BaseTextBox },
+  props: Object.keys(argTypes),
   methods: {
-    onInput: action('text', text)
+    onInput: action('input')
   },
-  template: `<BaseTextBox :place-holder="placeHolder" :is-invalid="isInvalid" :value="value" @text="onInput" />`
-}))
+  template: '<BaseTextBox :place-holder="placeHolder" :is-invalid="isInvalid" :value="value" @text="onInput" />',
+})
+
+withPlaceHolder.args =  {
+  placeHolder: '入力してください!'
+}
+
+export const isInvalid = () => ({
+  components: { BaseTextBox },
+  template: `<BaseTextBox place-holder="placeHolder" :is-invalid="true" value="" />`
+})
+
+export const hasValue = () => ({
+  components: { BaseTextBox },
+  template: `<BaseTextBox place-holder="placeHolder" :is-invalid="false" value="value" />`
+})
+
